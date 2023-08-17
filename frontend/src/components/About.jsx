04c6c54a90f054d4177b17d9c12f1e20870ceb07
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-08-17 22:42:39 +0530

    data fetch done

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import api from '../services/api';
+import './About.css';
 
 const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-    </div>
-  )
-}
+  const [aboutContent, setAboutContent] = useState(null);
 
-export default About
+  useEffect(() => {
+    // Fetch About content from API
+    api.get('/about')
+      .then(response => {
+        setAboutContent(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching About content:', error);
+      });
+  }, []);
+
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">About Us</h1>
+      {aboutContent && (
+        <div className="about-content">
+          <p className="content1">{aboutContent.content1}</p>
+          <p className="content2">{aboutContent.content2}</p>
+          <p className="content3">{aboutContent.content3}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default About;
--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -1,14 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import api from '../services/api';
+import './Shop.css';
 import { Card, CardContent, CardMedia, Typography, Grid } from '@mui/material';
 
 const Shop = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch Product content from API
+    api.get('/product')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching Product content:', error);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Shop</h1>
+      <h1>Products</h1>
+      <Grid container spacing={2}>
+        {products.map(product => (
+          <Grid item xs={12} sm={6} md={3} key={product._id}>
+            <Card sx={{ maxWidth: 300 }}>
+              {product.image && (
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={product.image}
+                  alt={`Flavour: ${product.name}`}
+                />
+              )}
+              <CardContent>
+                <Typography variant="h6" component="div" className='product-name'>
+                  {product.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.description}
+                </Typography>
+                {product.price && (
+                  <Typography variant="body2" color="text.secondary" className='price'>
+                    Price: {product.price}
+                  </Typography>
+                )}
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
 
-export default Shop
+export default Shop;
