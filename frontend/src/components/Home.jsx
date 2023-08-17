b35487cfb85c12296215b47f08d320ebfa35724c
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-08-17 22:53:45 +0530

    changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import api from '../services/api';
+import './Home.css';
 
 import HomeHero from './Hero/HomeHero/HomeHero';
 const Home = () => {
@@ -20,20 +21,33 @@ const Home = () => {
   return (
     <div>
       <HomeHero />
-      <div>
+      <div className="home-container">
         {homeContent && (
-          <div>
-            <h1>{homeContent.heading1}</h1>
-            <p>{homeContent.content1}</p>
-            <h2>{homeContent.heading2}</h2>
-            <p>{homeContent.content2}</p>
-            <img src={homeContent.image} alt="Home Content" />
+          <div className="home-content">
+            <h1 className="home-heading">{homeContent.heading1}</h1>
+            <p className="home-text">{homeContent.content1}</p>
+            <h2 className="home-subheading">{homeContent.heading2}</h2>
+            <p className="home-text">{homeContent.content2}</p>
+            <img
+              className="home-image"
+              src={homeContent.image}
+              alt="Home Content"
+            />
           </div>
         )}
       </div>
-
     </div>
   );
 };
 
 export default Home
+
+{/* {homeContent && (
+  <div>
+    <h1>{homeContent.heading1}</h1>
+    <p>{homeContent.content1}</p>
+    <h2>{homeContent.heading2}</h2>
+    <p>{homeContent.content2}</p>
+    <img src={homeContent.image} alt="Home Content" />
+  </div>
+)} */}
