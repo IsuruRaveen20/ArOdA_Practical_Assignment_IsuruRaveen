4ec82a4c1bf317c90488a961c8b1541c43d07dc9
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-08-17 09:50:19 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,3 +3,17 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+app.use(bodyParser.json());
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
