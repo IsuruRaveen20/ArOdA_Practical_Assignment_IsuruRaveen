c4d2e2faa8cce5b807c3ab8a6293769af7ad3273
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-08-17 13:37:43 +0530

    changes

--- backend/src/routes/productsRoutes.js
+++ backend/src/routes/productsRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { getAllProducts } = require("../controllers/productsController");
 
-//Create a New Router instance
+//Create a New Router Instance
 const router = express.Router();
+const productValidations = require('../validations/productValidations');
 
-router.get('/', getAllProducts);
+router.get('/', getProducts);
+router.post('/', productValidations, submitProduct);
 
 module.exports = router;
 
--- backend/src/validations/productValidations.js
+++ backend/src/validations/productValidations.js
@@ -1,9 +1,19 @@
-const { query } = require('express-validator');
+// const { query } = require('express-validator');
 
-const productFetchValidations = [
-    query('minPrice').optional().isString().withMessage('Min price must be a number'),
-    query('maxPrice').optional().isString().withMessage('Max price must be a number'),
+// const productFetchValidations = [
+//     query('minPrice').optional().isString().withMessage('Min price must be a number'),
+//     query('maxPrice').optional().isString().withMessage('Max price must be a number'),
+// ];
+
+const { body } = require('express-validator');
+
+const productValidations = [
+  body('name').notEmpty().withMessage('Name is required'),
+  body('description').notEmpty().withMessage('Description email format'),
+  body('image').notEmpty().withMessage('Image is required'),
+  body('price').notEmpty().withMessage('Price is required'),
 ];
 
-module.exports = productFetchValidations;
+
+module.exports = productValidations;
 
